b2a306299586ab7cf4e5031d9f1e26bcd8dea2b3
Nicolasnull <nnull31687@gmail.com>
2021-05-25 12:58:58 -0400

    cleaned up stuffs

--- DeepLearningUdacityCourse/section10_perceptron_algorithm.py
+++ DeepLearningUdacityCourse/section10_perceptron_algorithm.py
@@ -17,17 +17,16 @@ def prediction(X, W, b):
 # update the weights and bias W, b, according to the perceptron algorithm,
 # and return W and b.
 def perceptronStep(X, y, W, b, learn_rate = 0.01):
-
     for i in range(len(X)):
         y_hat = prediction(X[i],W,b)
         if y[i]-y_hat == 1:
             W[0] += X[i][0]*learn_rate
             W[1] += X[i][1]*learn_rate
             b += learn_rate
-        elif y[i]-y_hat == 0:
+        elif y[i]-y_hat == -1:
             W[0] -= X[i][0]*learn_rate
             W[1] -= X[i][1]*learn_rate
-            b -= learn_rate*W[0] + X[i][1] * W[1] + b
+            b -= learn_rate
     return W, b
     
 # This function runs the perceptron algorithm repeatedly on the dataset,
@@ -57,6 +56,31 @@ y = data_frame[['2']]
 y = y.to_numpy()
 
 lines = trainPerceptronAlgorithm(X, y)
-print(lines[0][1])
 
-# Try to plot this using MatPlotLib
+# Plotting the solution boundary
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#Some helper functions for plotting and drawing lines (from their github)
+
+def plot_points(X, y):
+    admitted = X[np.argwhere(y==1)]
+    rejected = X[np.argwhere(y==0)]
+    plt.scatter([s[0][0] for s in rejected], [s[0][1] for s in rejected], s = 25, color = 'blue', edgecolor = 'k')
+    plt.scatter([s[0][0] for s in admitted], [s[0][1] for s in admitted], s = 25, color = 'red', edgecolor = 'k')
+
+def display(m, b, color='g--'):
+    plt.xlim(-0.05,1.05)
+    plt.ylim(-0.05,1.05)
+    x = np.arange(-10, 10, 0.1)
+    plt.plot(x, m*x+b, color)
+    
+import matplotlib.pyplot as plt
+data = pd.read_csv('section10_data.csv', header=None)
+X = np.array(data[[0,1]])
+y = np.array(data[2])
+plot_points(X,y)
+for line in lines:
+    display(line[0],line[1])
+plt.show()
